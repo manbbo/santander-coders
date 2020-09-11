4d0a77ed0c1f6489821cfe48260b63bd73b7d3a5
Andre Piton <andre.elias.vieira@gmail.com>
2020-09-11 16:56:58 -0300

    Adding vararg and implementing

--- SantanderProjects/src/br/com/digitalhouse/thirdproject/Impressora.kt
+++ SantanderProjects/src/br/com/digitalhouse/thirdproject/Impressora.kt
@@ -3,13 +3,14 @@ package br.com.digitalhouse.thirdproject
 class Impressora {
     val listaImprimivel = mutableListOf<Imprimivel>()
 
-    fun addImprimivel(item: Imprimivel) {
-        listaImprimivel.add(item)
-    }
+    fun addImprimivel(vararg item: Imprimivel) = listaImprimivel.addAll(item)
+
+    fun addImprimivel(item: Imprimivel) = listaImprimivel.add(item)
 
     fun printAll() {
         for (i in listaImprimivel) {
             i.imprimir()
         }
+        listaImprimivel.clear()
     }
 }
--- SantanderProjects/src/br/com/digitalhouse/thirdproject/Main.kt
+++ SantanderProjects/src/br/com/digitalhouse/thirdproject/Main.kt
@@ -1,10 +1,22 @@
 package br.com.digitalhouse.thirdproject
 
 fun main() {
-    val impressora = Impressora()
-    impressora.addImprimivel(Foto("img.jpg", "FOTO"))
-    impressora.addImprimivel(Documento("img.jpg", "DOCUMENTO"))
-    impressora.addImprimivel(Contrato("img.jpg", "CONTRATO"))
+    //Primeiro jeito
+    val impressora1 = Impressora()
+    impressora1.addImprimivel(
+            Foto("img.jpg", "JPG"),
+            Documento("doc.docx", "DOCX"),
+            Contrato("contrato.pdf", "PDF"))
 
-    impressora.printAll()
+    impressora1.printAll()
+    
+    //Segundo jeito
+    val impressora2 = Impressora()
+    val foto = Foto("img.jpg", "JPG")
+    val doc = Documento("doc.docx", "DOCX")
+    val contrato = Contrato("contrato.pdf", "PDF")
+
+    impressora2.addImprimivel(foto, doc, contrato)
+
+    impressora2.printAll()
 }

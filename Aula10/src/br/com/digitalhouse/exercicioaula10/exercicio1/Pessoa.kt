f4c2a410430c779ccc6208b4cb5e77defba07346
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-09-18 15:58:27 -0300

    Adição dos exercicios 1, 2 e 4

---
+++ Aula10/src/br/com/digitalhouse/exercicioaula10/exercicio1/Pessoa.kt
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.exercicioaula10.exercicio1
+
+//------------------------------EX1-----------------------------------
+
+// 1    Criar a classe Pessoa que tenha como propriedades nome (String) e RG(Integer)
+// 2    Na classe Main criar duas pessoas com o mesmo numero de RG
+// 3    Usando equals, comparar se a primeira pessoa criada é igual à segunda.
+//          Qual é o resultado?
+// 4    Sobrescrever o equals da classe Pessoa para que compare apenas o número de RG.
+
+
+data class Pessoa(var nome: String, var RG: Int) {
+}

176065efab8e9d67b6c31f08b19760bcae1747b4
Piton <andre.elias.vieira@gmail.com>
2020-10-02 22:26:00 -0300

    Creating a Simple Game in Kotlin - Guessing numbers

--- Android/SantanderAndroid/app/src/main/java/br/com/digitalhouse/santanderproject/MainActivity.kt
+++ Android/SantanderAndroid/app/src/main/java/br/com/digitalhouse/santanderproject/MainActivity.kt
@@ -1,11 +1,19 @@
 package br.com.digitalhouse.santanderproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var tag: String = "Main"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        play.setOnClickListener{
+            startActivity(Intent(this, GameActivity().javaClass))
+        }
     }
 }

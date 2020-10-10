e790afdcad5eaac2123f8b06bc46440d0b83c62a
Piton <andre.elias.vieira@gmail.com>
2020-10-09 22:41:10 -0300

    Second commit

--- Android/app/src/main/java/br/com/digitalhouse/alcoolgasoza/MainActivity.kt
+++ Android/app/src/main/java/br/com/digitalhouse/alcoolgasoza/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.digitalhouse.alcoolgasoza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
-        comparar.setOnClickListener{
-            if (etanol)
-        }
+        comparar.setOnClickListener(
+            object : View.OnClickListener{
+                override fun onClick(v: View?) {
+
+                    if (etanol.text.toString().toFloat() <
+                        (gasosa.toString().toFloat() * 0.7)) {
+                        compre.setImageResource(R.drawable.abasteca_etanol)
+
+                    } else {
+                        compre.setImageResource(R.drawable.abasteca_gasolina)
+                    }
+                }})
     }
 }

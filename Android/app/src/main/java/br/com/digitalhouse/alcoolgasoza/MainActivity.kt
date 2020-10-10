df081bc17ad2b1991bf96c71a76e9dbef59bb28a
Piton <andre.elias.vieira@gmail.com>
2020-10-09 22:48:21 -0300

    Adding exercise from today's class

--- Android/app/src/main/java/br/com/digitalhouse/alcoolgasoza/MainActivity.kt
+++ Android/app/src/main/java/br/com/digitalhouse/alcoolgasoza/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.digitalhouse.alcoolgasoza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +10,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
-        comparar.setOnClickListener{
-            if (etanol)
-        }
+        comparar.setOnClickListener(
+            object : View.OnClickListener{
+                override fun onClick(v: View?) {
+                    var valorEtanol = etanol.text.toString().trim().toFloat()
+                    var valorGasosa = gasosa.text.toString().trim().toFloat()
+
+                    if (valorEtanol <
+                        (valorGasosa * 0.7)) {
+                        compre.setImageResource(R.drawable.abasteca_etanol)
+
+                    } else {
+                        compre.setImageResource(R.drawable.abasteca_gasolina)
+                    }
+                }})
     }
 }

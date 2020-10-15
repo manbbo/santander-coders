e7dbe6a7e635fe87da80873dc9caaa3e3a513532
Piton <andre.elias.vieira@gmail.com>
2020-10-14 21:28:45 -0300

    Tela de cadastro feita

--- Desafio4/app/src/main/java/com/example/snackbar/CadastroActivity.kt
+++ Desafio4/app/src/main/java/com/example/snackbar/CadastroActivity.kt
@@ -2,10 +2,16 @@ package com.example.snackbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.toolbar.*
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+
+        setSupportActionBar(my_toolbar)
+        my_toolbar.title = "CADASTRAR"
+        my_toolbar.navigationIcon = ContextCompat.getDrawable(this, R.drawable.ic_backbt)
     }
 }

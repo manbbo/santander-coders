9104919ba1425d11887797d659a5109a3bd0ff2b
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2020-10-16 01:40:35 -0300

    Cadastro e consistência de entradas

--- Desafio4/app/src/main/java/com/example/snackbar/CadastroActivity.kt
+++ Desafio4/app/src/main/java/com/example/snackbar/CadastroActivity.kt
@@ -1,9 +1,23 @@
 package com.example.snackbar
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Menu
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import com.example.snackbar.domain.Usuario
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_cadastro.*
+import kotlinx.android.synthetic.main.cadastro_body.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 class CadastroActivity : AppCompatActivity() {
@@ -19,6 +33,52 @@ class CadastroActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        edUserName_cad.setText(intent.extras?.get("username").toString(), TextView.BufferType.EDITABLE)
+        edPassword_cad.setText(intent.extras?.get("password").toString(), TextView.BufferType.EDITABLE)
+
+        var pass = edPassword_cad.text
+        var username = edUserName_cad.text
+        var email = edEmail_cad.text
+        var passRepet = edPasswordRepeat_cad.text
+
+        btn_cadastrar.setOnClickListener {
+            if(username == null || pass == null || email == null || passRepet == null){
+                Toast.makeText(this, "Digite novamente as informações!", Toast.LENGTH_SHORT).show()
+            }else if (pass.isEmpty() || username.isEmpty() || email.isEmpty() || passRepet.isEmpty()){
+                Snackbar.make(act_cadastro, "Complete os campos destacados", Snackbar.LENGTH_LONG)
+                    .setAction("Ok", View.OnClickListener {}).show()
+
+                if (pass.isEmpty()) edPassword_cad.backgroundTintList = ContextCompat.getColorStateList(this, R.color.colorRed)
+                else edPassword_cad.backgroundTintList = ContextCompat.getColorStateList(this, R.color.colorAccent)
+
+                if (username.isEmpty()) edUserName_cad.backgroundTintList = ContextCompat.getColorStateList(this, R.color.colorRed)
+                else edUserName_cad.backgroundTintList = ContextCompat.getColorStateList(this, R.color.colorAccent)
+
+                if (passRepet.isEmpty()) edPasswordRepeat_cad.backgroundTintList = ContextCompat.getColorStateList(this, R.color.colorRed)
+                else edPasswordRepeat_cad.backgroundTintList = ContextCompat.getColorStateList(this, R.color.colorAccent)
+
+                if (email.isEmpty()) edEmail_cad.backgroundTintList = ContextCompat.getColorStateList(this, R.color.colorRed)
+                else edEmail_cad.backgroundTintList  = ContextCompat.getColorStateList(this, R.color.colorAccent)
+            } else if (edPassword_cad.text.toString() != edPasswordRepeat_cad.text.toString()) {
+                edPasswordRepeat_cad.text?.clear()
+                Snackbar.make(act_cadastro, "Repita a senha corretamente", Snackbar.LENGTH_LONG)
+                    .setAction("Ok", View.OnClickListener {}).show()
+            }else{
+                Toast.makeText(this, "Usuário '${edUserName_cad.text.toString()}' realizou Login", Toast.LENGTH_SHORT).show()
+
+                var intent = Intent(this, MainActivity::class.java)
+
+                intent.putExtra("user", Usuario(0,
+                    edUserName_cad.text.toString(),
+                    edPassword_cad.text.toString()).username)
+                startActivity(intent)
+            }
+        }
+
+        my_toolbar.setNavigationOnClickListener {
+            finish()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
---
+++ Desafio4/app/src/main/java/com/example/snackbar/LoginActivity.kt
@@ -0,0 +1,62 @@
+package com.example.snackbar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.snackbar.domain.Usuario
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_cadastro.*
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.login_body.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btnImg_Google.setOnClickListener {
+            Toast.makeText(this, "Google+", Toast.LENGTH_SHORT).show()
+        }
+
+        btnImg_Face.setOnClickListener {
+            Toast.makeText(this, "Facebook", Toast.LENGTH_SHORT).show()
+        }
+
+        btnImg_Insta.setOnClickListener {
+            Toast.makeText(this, "Instagram", Toast.LENGTH_SHORT).show()
+        }
+
+        btnLogin.setOnClickListener {
+            if (edPassword.text != null && edPassword.text.toString() != "123"){
+                Snackbar.make(act_login, "Senha incorreta", Snackbar.LENGTH_LONG)
+                    .setAction("Ok", View.OnClickListener {}).show()
+            }else{
+                var intent = Intent(this, MainActivity::class.java)
+
+                intent.putExtra("user", Usuario(0,
+                edUserName.text.toString(),
+                edPassword.text.toString()).username)
+                startActivity(intent)
+
+                Toast.makeText(this, "Usuário '${edUserName.text.toString()}' realizou Login", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        txv_cadastro.setOnClickListener {
+            var intent = Intent(this, CadastroActivity::class.java)
+            intent.putExtra("username", edUserName.text.toString())
+            intent.putExtra("password", edPassword.text.toString())
+
+            startActivity(intent)
+
+            Toast.makeText(this, "Cadastro de Usuário", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onStop() {
+
+        super.onStop()
+    }
+}

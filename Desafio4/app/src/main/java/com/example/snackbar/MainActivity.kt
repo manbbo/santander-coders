3dfd40c88d0222e46a29667ebda152d4652185a1
Piton <andre.elias.vieira@gmail.com>
2020-10-16 17:20:59 -0300

    Adding challenge 4 to the master stream

---
+++ Desafio4/app/src/main/java/com/example/snackbar/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.snackbar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent().extras
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ Desafio4/app/src/main/java/com/example/snackbar/domain/Usuario.kt
@@ -0,0 +1,7 @@
+package com.example.snackbar.domain
+
+import java.io.Serializable
+
+public class Usuario (val id: Int, val username: String, val senha: String) : Serializable {
+
+}

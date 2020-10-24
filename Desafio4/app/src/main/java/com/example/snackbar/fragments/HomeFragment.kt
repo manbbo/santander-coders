d80e5b1aee2a2fd510aed24c010f7c605f712fe1
Piton <andre.elias.vieira@gmail.com>
2020-10-23 22:32:00 -0300

    updated desafio4

---
+++ Desafio4/app/src/main/java/com/example/snackbar/fragments/HomeFragment.kt
@@ -0,0 +1,59 @@
+package com.example.snackbar.fragments
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.provider.CalendarContract
+import android.text.Layout
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.toColor
+import com.example.snackbar.R
+import kotlinx.android.synthetic.main.cards_layout.*
+
+
+class HomeFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        //var bt_Sem = R.id.sem
+        //var bt_Mes = R.id.mes
+        //var bt_Ano = R.id.ano
+
+        //var l_card = R.id.card
+        //var l_card_title = R.id.card_title
+        //var l_card_value = R.id.card_value
+        //var l_card_lancamento = R.id.card_lancamento
+
+
+        return view
+
+        card!!.setBackgroundColor(ContextCompat.getColor(card!!.context,
+            R.color.colorOrange))
+        card_title!!.text = "Gastos"
+    }
+
+    companion object {
+        fun newInstance() =
+                HomeFragment().apply {
+                    arguments = Bundle().apply {
+
+                    }
+                }
+    }
+}
---
+++ Desafio4/app/src/main/java/com/example/snackbar/ui/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.snackbar.ui
+
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.PorterDuff
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.snackbar.fragments.MyAdapter
+import com.example.snackbar.R
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent().extras
+        setContentView(R.layout.activity_main)
+
+        my_toolbar.title = "MyWallet"
+        my_toolbar.navigationIcon = ContextCompat.getDrawable(this, R.drawable.ic_backbt)
+
+        setSupportActionBar(my_toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        my_toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+        var navigationView = tablayout
+        var viewPager = fragment
+
+        navigationView!!.addTab(navigationView.newTab().setText("Home"))
+        navigationView!!.getTabAt(0)!!.setIcon(R.drawable.ic_home)
+        navigationView!!.getTabAt(0)!!.icon!!.setColorFilter(ContextCompat.getColor(
+                navigationView.context, R.color.colorRed),
+                PorterDuff.Mode.SRC_IN)
+
+        navigationView!!.addTab(navigationView.newTab().setText("Entradas"))
+        navigationView!!.getTabAt(1)!!.setIcon(R.drawable.ic_entries)
+
+        navigationView!!.addTab(navigationView.newTab().setText("Gastos"))
+        navigationView!!.getTabAt(2)!!.setIcon(R.drawable.ic_money)
+
+        navigationView!!.isInlineLabel = true
+        navigationView.tabGravity = TabLayout.GRAVITY_FILL
+        navigationView.setSelectedTabIndicatorColor(Color.WHITE)
+
+        var adapter = MyAdapter(this,
+            supportFragmentManager,
+            navigationView!!.tabCount)
+        viewPager!!.adapter = adapter
+
+        viewPager!!.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(navigationView!!))
+
+        navigationView!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewPager!!.currentItem = tab.position
+
+                val tabIconColor = ContextCompat.getColor(
+                        navigationView.context, R.color.colorRed)
+
+                tab.icon!!.setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN)
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                val tabIconColor = ContextCompat.getColor(
+                        navigationView.context, R.color.colorWhite)
+
+                tab.icon!!.setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN)
+            }
+            override fun onTabReselected(tab: TabLayout.Tab) {
+                val tabIconColor = ContextCompat.getColor(
+                        navigationView.context, R.color.colorRed)
+
+                tab.icon!!.setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN)
+            }
+        })
+    }
+}

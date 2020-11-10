2e03c296adcd844db8ef21b3df561ddaebf4cffb
Piton <andre.elias.vieira@gmail.com>
2020-11-10 17:00:39 -0300

    Added bt_back on PlateDetailActivity.kt

--- DesafioAndroidCore/app/src/main/java/br/com/digitalhouse/desafioandroidcore/PlateDetailActivity.kt
+++ DesafioAndroidCore/app/src/main/java/br/com/digitalhouse/desafioandroidcore/PlateDetailActivity.kt
@@ -2,10 +2,15 @@ package br.com.digitalhouse.desafioandroidcore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_food_details.*
 
 class PlateDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plate_detail)
+
+        bt_back.setOnClickListener {
+            finish()
+        }
     }
 }

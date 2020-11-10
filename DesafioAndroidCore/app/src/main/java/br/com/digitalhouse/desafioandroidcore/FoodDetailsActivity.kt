e3d4847056be01d6667fca2233a2db73dc598699
Piton <andre.elias.vieira@gmail.com>
2020-11-10 16:59:06 -0300

    Added PlateDetailActivity.kt, fixed FoodDetailsActivity.kt and made another adapter, FoodDetailedAdapter.kt. Also created activity_food_details.xml and activity_plate_detail.xml for the activities, and detailed_food_item.xml for the adapter created

--- DesafioAndroidCore/app/src/main/java/br/com/digitalhouse/desafioandroidcore/FoodDetailsActivity.kt
+++ DesafioAndroidCore/app/src/main/java/br/com/digitalhouse/desafioandroidcore/FoodDetailsActivity.kt
@@ -2,7 +2,12 @@ package br.com.digitalhouse.desafioandroidcore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import br.com.digitalhouse.desafioandroidcore.adapters.FoodDetailedAdapter
+import br.com.digitalhouse.desafioandroidcore.data.FoodCardInfo
 import kotlinx.android.synthetic.main.activity_food_details.*
+import kotlinx.android.synthetic.main.activity_home.*
 
 class FoodDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +17,21 @@ class FoodDetailsActivity : AppCompatActivity() {
         bt_back.setOnClickListener {
             finish()
         }
+
+        val cardapio = arrayListOf<FoodCardInfo>(
+                FoodCardInfo("Salada com molho de Gengibre", "Av. Lavandisca, 717 - Indianópolis, São Paulo", "Fecha às 22:00", R.drawable.image4),
+                FoodCardInfo("Salada com molho de Gengibre", "Alameda dos Arapanés, 532 - Moema", "Fecha às 00:00", R.drawable.image4),
+                FoodCardInfo("Salada com molho de Gengibre", "Av. Moaci, 187, 187 - Moema, São Paulo", "Fecha às 00:00", R.drawable.image4),
+                FoodCardInfo("Salada com molho de Gengibre", "Alameda Jauaperi, 626 - Moema", "Fecha às 01:00", R.drawable.image4),
+                FoodCardInfo("Salada com molho de Gengibre", "Av. Lavandisca, 717 - Indianópolis, São Paulo", "Fecha às 22:00", R.drawable.image4),
+                FoodCardInfo("Salada com molho de Gengibre", "Alameda dos Arapanés, 532 - Moema", "Fecha às 00:00", R.drawable.image4),
+                FoodCardInfo("Salada com molho de Gengibre", "Av. Moaci, 187, 187 - Moema, São Paulo", "Fecha às 00:00", R.drawable.image4),
+                FoodCardInfo("Salada com molho de Gengibre", "Alameda Jauaperi, 626 - Moema", "Fecha às 01:00", R.drawable.image4),
+                FoodCardInfo("Salada com molho de Gengibre", "Av. Moaci, 187, 187 - Moema, São Paulo", "Fecha às 00:00", R.drawable.image4),
+                FoodCardInfo("Salada com molho de Gengibre", "Alameda Jauaperi, 626 - Moema", "Fecha às 01:00", R.drawable.image4)
+        )
+
+        rc_view_twocolumn.adapter = FoodDetailedAdapter(cardapio)
+        rc_view_twocolumn.layoutManager = GridLayoutManager(this, 2)
     }
 }
---
+++ DesafioAndroidCore/app/src/main/java/br/com/digitalhouse/desafioandroidcore/PlateDetailActivity.kt
@@ -0,0 +1,11 @@
+package br.com.digitalhouse.desafioandroidcore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlateDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plate_detail)
+    }
+}

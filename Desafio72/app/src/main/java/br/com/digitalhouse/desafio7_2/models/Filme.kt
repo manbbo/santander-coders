3e5b5ee3d3b4d87572732c26e97bf842e188f3bf
Piton <andre.elias.vieira@gmail.com>
2020-12-04 16:08:58 -0300

    Adicionando o desafio

--- Desafio72/app/src/main/java/br/com/digitalhouse/desafio7_2/models/Filme.kt
+++
@@ -1,3 +0,0 @@
-package br.com.digitalhouse.desafio7_2.models
-
-data class Filme (var titulo : String, var sinopse: String, var id: Int)
---
+++ webservices/app/src/main/java/br/com/digitalhouse/webservices/models/Date.kt
@@ -0,0 +1,6 @@
+package br.com.digitalhouse.webservices.models
+
+data class Date(
+    val date: String,
+    val type: String
+)

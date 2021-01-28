faa07b31f68291376876a8f377226af1417085cb
manbbo <manbbo@outlook.com>
2021-01-28 16:07:33 -0300

    Adding Storage and Image selector

--- DesafioFinal/app/src/main/java/com/example/desafiofinal/api/Storage.kt
+++ DesafioFinal/app/src/main/java/com/example/desafiofinal/api/Storage.kt
@@ -1,17 +1,40 @@
 package com.example.desafiofinal.api
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.widget.ImageView
 import androidx.core.app.ActivityCompat.startActivityForResult
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import com.example.desafiofinal.api.Credentials
 import com.google.firebase.storage.StorageReference
+import java.io.ByteArrayOutputStream
 
 class Storage {
-    fun beginStore() {
-        var store = Firebase.storage(Credentials().URL)
-        var storageRef = store.reference
-        var imagesRef: StorageReference? = storageRef.child("image")
+    var store = Firebase.storage(Credentials().URL)
+    var storageRef = store.reference
+    var imagesRef = storageRef.child("image")
 
+    fun uploadFromMemory(imageView : ImageView) : String {
+        var imagePath = ""
+// Get the data from an ImageView as bytes
+        imageView.isDrawingCacheEnabled = true
+        imageView.buildDrawingCache()
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = imagesRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+            imagePath = taskSnapshot.metadata!!.path
+        }
+
+        return imagePath
     }
 }
--- DesafioFinal/app/src/main/java/com/example/desafiofinal/ui/GameInfoFillActivity.kt
+++ DesafioFinal/app/src/main/java/com/example/desafiofinal/ui/GameInfoFillActivity.kt
@@ -1,7 +1,9 @@
 package com.example.desafiofinal.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
@@ -11,6 +13,9 @@ import com.example.desafiofinal.R
 import com.example.desafiofinal.data.GameTileInfo
 
 class GameInfoFillActivity : AppCompatActivity() {
+    private val REQUEST_TAKE_PHOTO = 0
+    private val REQUEST_SELECT_IMAGE_IN_ALBUM = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_info_fill)
@@ -18,12 +23,12 @@ class GameInfoFillActivity : AppCompatActivity() {
         val bundle: Bundle? = intent.extras
         val game = bundle?.getSerializable("game") as? GameTileInfo
 
-        if (game != null) {
-            val title : EditText = findViewById(R.id.game_nm)
-            val image: ImageView = findViewById(R.id.game_img)
-            val year: EditText = findViewById(R.id.game_yr)
-            val description: EditText = findViewById(R.id.game_descr)
+        val title : EditText = findViewById(R.id.game_nm)
+        val image: ImageView = findViewById(R.id.game_img)
+        val year: EditText = findViewById(R.id.game_yr)
+        val description: EditText = findViewById(R.id.game_descr)
 
+        if (game != null) {
             title.setText(game.name)
             year.setText(game.year.toString())
             description.setText(game.description)
@@ -32,6 +37,25 @@ class GameInfoFillActivity : AppCompatActivity() {
                 .load(game.photo)
                 .placeholder(R.drawable.ic_baseline_photo_camera_24)
                 .into(image)
+        }
+
+        image.setOnClickListener{
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == REQUEST_SELECT_IMAGE_IN_ALBUM) {
+            val imageUri = data?.data
+            val image : ImageView = findViewById(R.id.game_img)
+
+            image.setImageURI(imageUri)
         }
     }
 }

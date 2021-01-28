24a418689614fb43a63e233b6745e9b1632396dd
manbbo <manbbo@outlook.com>
2021-01-28 18:48:52 -0300

    Credentials EXAMPLE

---
+++ DesafioFinal/app/src/main/java/com/example/desafiofinal/api/CredentialsExample.kt
@@ -0,0 +1,10 @@
+package com.example.desafiofinal.api
+
+class CredentialsExample {
+    val URL : String
+        get() = "<<The url of your STORAGE>>"
+    val imageURL : String
+        get() = "https://firebasestorage.googleapis.com/v0/b/<<Your URL>>/o/"
+    /* The image url can be found on your
+     STORAGE, when you open an Image*/
+}
